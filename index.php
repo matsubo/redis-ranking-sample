84d7eeeeb268dd3b99726365281bc64a909a9bb5
Yuki Matsukura <matsubokkuri@gmail.com>
2015-11-27 19:28:54 +0900

    adjustment placement

--- index.php
+++ index.php
@@ -7,26 +7,19 @@
     <meta name="description" content="">
     <meta name="author" content="Yuki Matsukura">
 
-    <!-- Le styles -->
-    <link href="./components/bootstrap/docs/assets/css/bootstrap.css" rel="stylesheet">
-    <style>
-      body {
-        padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
-      }
-    </style>
-    <link href="./components/bootstrap/docs/assets/css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
-      <script src="components/bootstrap/docs/assets/js/html5shiv.js"></script>
+      <script src="assets/js/html5shiv.js"></script>
     <![endif]-->
 
     <!-- Fav and touch icons -->
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="components/bootstrap/docs/assets/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="components/bootstrap/docs/assets/ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="components/bootstrap/docs/assets/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="components/bootstrap/docs/assets/ico/apple-touch-icon-57-precomposed.png">
-    <link rel="shortcut icon" href="components/bootstrap/docs/assets/ico/favicon.png">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
+    <link rel="shortcut icon" href="assets/ico/favicon.png">
   </head>
 
   <body>
@@ -40,23 +33,25 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 })(window,document,'script','dataLayer','GTM-WNH3R2');</script>
 <!-- End Google Tag Manager -->
 
-    <div class="navbar navbar-inverse navbar-fixed-top">
-      <div class="navbar-inner">
-        <div class="container">
-          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="brand" href="?">AKB勝手に総選挙</a>
-          <div class="nav-collapse collapse">
-            <ul class="nav">
-              <li class="active"><a href="?">Home</a></li>
-            </ul>
-          </div><!--/.nav-collapse -->
-        </div>
-      </div>
+
+<nav class="navbar navbar-inverse navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="?">AKB勝手に総選挙</a>
     </div>
+
+  </div><!-- /.container-fluid -->
+</nav>
+
+
+
 
     <div class="container">
 
@@ -71,6 +66,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 <th>順位</th><th>名前</th><th>得票数</th><th>&nbsp;</th>
 </tr>
 
+
 <?php
 
 require_once __DIR__ . '/vendor/autoload.php';
@@ -82,21 +78,6 @@ $redis->connect('127.0.0.1', 6379);
 
 $ranking = new Ranking($key = 'akb', $redis);
 
-
-/* initialize code.
-$member_text = file_get_contents('member.txt');
-$member_array = explode("\n", $member_text);
-foreach ($member_array as $member) {
-    $member = trim($member);
-    if (!$member) {
-        continue;
-    }
-    if ($ranking->getScore($member) !== false) {
-        continue;
-    }
-    $ranking->setUserScore($member, 0);
-}
- */
 $text = isset($_REQUEST['text']) ? $_REQUEST['text'] : '';
 if (isset($_REQUEST['mode']) && $_REQUEST['mode']  == 'increment') {
     if ($ranking->getScore($text) !== false) {
@@ -122,17 +103,6 @@ foreach ($ranking->getRange(0, -1, true) as $name => $value) {
 <a href="https://github.com/matsubo/redis-ranking-sample/blob/master/member.txt">https://github.com/matsubo/redis-ranking-sample/blob/master/member.txt</a>
 </p>
 
-<!--
-<form method="post" action="?mode=add">
-  <fieldset>
-    <legend>追加</legend>
-    <label>名前</label>
-    <input name="text" type="text" placeholder="前田敦子">
-    <button type="submit" class="btn">追加</button>
-  </fieldset>
-</form>
--->
-
 
 <h2>メモ</h2>
 <a href="http://matsu.teraren.com/blog/2013/04/28/redis-ranking-api/">Redis-Ranking</a>を使って構築しています。
@@ -143,13 +113,16 @@ foreach ($ranking->getRange(0, -1, true) as $name => $value) {
 <br />
 <br />
 
+<footer>
+<div class="row">
+<div class="col-md-12 text-right">
+<a href="http://matsu.teraren.com/blog/">&copy Yuki Matsukura</a>
+</div>
+</footer>
 
     </div> <!-- /container -->
 
-    <!-- Le javascript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="components/bootstrap/docs/assets/js/jquery.js"></script>
+    <script src="bower_components/jquery/dist/jquery.min.js"></script>
 
   </body>
 </html>
