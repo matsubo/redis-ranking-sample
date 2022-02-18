ca79fe65394fc390f4a2aabbeea1f827c1e464ab
Yuki Matsukura <matsubokkuri@gmail.com>
2022-02-18 00:38:42 +0000

    update adsense code

--- ad.inc.php
+++ ad.inc.php
@@ -1,12 +1,12 @@
-
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- AKB ranking -->
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3739212741143912"
+     crossorigin="anonymous"></script>
+<!-- ranking page -->
 <ins class="adsbygoogle"
      style="display:block"
      data-ad-client="ca-pub-3739212741143912"
-     data-ad-slot="3146672918"
-     data-ad-format="auto"></ins>
+     data-ad-slot="3405257928"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
 <script>
-(adsbygoogle = window.adsbygoogle || []).push({});
+     (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
-
