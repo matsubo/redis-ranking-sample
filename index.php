89df9c6d0d54c4519125263c5f2e09828c6f7769
Yuki Matsukura <matsubokkuri@gmail.com>
2023-09-12 15:41:37 +0900

    removed jquery

--- index.php
+++ index.php
@@ -150,7 +150,6 @@ foreach ($ranking->getRange(0, -1, true) as $name => $value) {
 </div>
 
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" referrerpolicy="no-referrer"></script>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.5/umd/popper.min.js" integrity="sha512-8cU710tp3iH9RniUh6fq5zJsGnjLzOWLWdZqBMLtqaoZUA6AWIE34lwMB3ipUNiTBP5jEZKY95SfbNnQ8cCKvA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
